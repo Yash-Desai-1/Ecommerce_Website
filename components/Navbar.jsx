0435a823f87977aee5be3f8681bad5e7c79de6b5
Yash-Desai-1 <yashdesai4320@gmail.com>
2023-01-15 12:34:13 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import {Cart} from './'
+import { useStateContext } from '../context/StateContext'
+import {AiOutlineShopping} from 'react-icons/ai'
+const Navbar = () => {
+  const {setShowCart,showCart,totalQuantities} = useStateContext();
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>
+          BeatPro Enterprises
+        </Link>
+      </p>
+      <button type='button'
+      className='cart-icon' onClick={()=>setShowCart(true)}>
+        <AiOutlineShopping/>
+        <span className='cart-item-qty'>{totalQuantities}</span>
+      </button>
+      {showCart && <Cart/>}
+    </div>
+  )
+}
+
+export default Navbar
---
+++ sanityecomm/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '02kpgx18',
+    dataset: 'production'
+  }
+})
